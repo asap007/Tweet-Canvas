c2d43e41cebded216db3f44d5fcdbbb2047f8ff4
asap007 <anshulofficial1202@gmail.com>
2024-09-23 00:21:01 +0530

    changed pupteer launch for deployement

--- app.js
+++ app.js
@@ -122,7 +122,11 @@ async function scrapeTwitterProfileWithRetry(username, maxRetries = 3) {
 }
 
 async function scrapeTwitterProfile(username) {
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || '/usr/bin/google-chrome-stable'
+  });  
   const page = await browser.newPage();
   try {
     console.log(`Navigating to https://twitter.com/${username}`);
